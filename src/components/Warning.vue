276e3988b4d5227c3ed9118c8190d78d355126af
NaomiK <naomi.kurata@web.de>
2020-12-12 16:41:11 +0100

    add component file

---
+++ src/components/Warning.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="ui icon red large message" v-show="isMessage">
+    <i class="bullhorn icon"></i>
+    <div class="content">
+        <div class="header">
+        SORRY! The request cannot be completed because you have exceeded the quota.
+        </div>
+        <p>Try again tomorrow!</p>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Warning',
+    props: ['isMessage']
+}
+</script>
